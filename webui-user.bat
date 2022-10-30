5a7a50a529a219de136e8905691a82c561fb8415
Buckzor <bucklogos@yahoo.co.uk>
2022-10-30 12:52:50 +0000

    ADD VAE

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers
+set COMMANDLINE_ARGS= --xformers --vae-path "\models\Stable-diffusion\vae-ft-mse-840000-ema-pruned.pt"
 
 call webui.bat
