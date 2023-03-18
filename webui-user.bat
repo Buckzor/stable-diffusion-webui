06776dbb7ab6cee6505d01ed582f4f415a2cfabd
Buckzor <bucklogos@yahoo.co.uk>
2023-03-18 13:32:47 +0000

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --vae-path "\models\Stable-diffusion\vae-ft-mse-840000-ema-pruned.pt"
+set COMMANDLINE_ARGS= --xformers
 
 call webui.bat
